d7dadf36b64f6667fc0fd80c37f996db7423dfa8
Sofia Lucca s302223 <s302223@studenti.polito.it>
2022-03-03 17:21:14 +0100

    Added text and file for first exercise

---
+++ Exercises/1_1score.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const score = [25, 30, 18, 27, 28, 27, 30, 26]; //define array with array literal
+    //literal is  something that doesn't change in a programming language
+
+/*primitive are usually defined with literals while other types are not defined usually with it
+    in JS Array and Objects can be defined with literals without use of functions
+
+
+*/
